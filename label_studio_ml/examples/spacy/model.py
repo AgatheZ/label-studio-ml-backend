e9b20b290a74ac01d136e0dd4a176d91a2f8ecb7
Agathe Zecevic <85028272+AgatheZ@users.noreply.github.com>
2024-10-08 14:20:56 +0100

    Add custom labels mapping for gastric atrophy

--- label_studio_ml/examples/spacy/model.py
+++ label_studio_ml/examples/spacy/model.py
@@ -11,7 +11,24 @@ nlp = spacy.load(SPACY_MODEL)
 class SpacyMLBackend(LabelStudioMLBase):
 
     # define your custom labels mapping here
-    _custom_labels_mapping = {}
+    _custom_labels_mapping = {  
+        "intestinal_metaplasia": "intestinal_metaplasia",
+        "negation": "negation",
+        "normal": "normal",
+        "GOJ": "GOJ",
+        "antrum": "antrum",
+        "pylorus": "pylorus",
+        "incisura": "incisura",
+        "body": "body",
+        "stomach_NOS": "stomach_NOS",
+        "cardia": "cardia",
+        "atrophy": "atrophy",
+        "intestinal metaplasia": "intestinal metaplasia",
+        "non-specialised": "non-specialised",
+        "cancerOrDysplasia": "cancerOrDysplasia",
+        "Mixed_NSandSpec": "Mixed_NSandSpec",
+        "adenoma": "adenoma"
+    }
 
     def setup(self):
         self.set("model_version", f'{self.__class__.__name__}-v0.0.1')
